f2c865e0138c96509b23022a1b4cf8651363cda1
brian432 <brian_cabrera123@outlook.es>
2023-02-16 19:17:15 -0300

    detalles

--- src/componentes/carousel/carousel.js
+++ src/componentes/carousel/carousel.js
@@ -19,7 +19,7 @@ export const Carousel = () => {
             <div
                 className='glitch-img div-absolute'
                 style={{
-                    backgroundImage: `url(images/${isPhone ? imagen + "-phone" : isTablet ? imagen + "-tablet" : imagen}.jpg)`,
+                    backgroundImage: `url(images/${isPhone ? imagen + "-phone" : isTablet ? imagen + "-tablet" : imagen}.webp)`,
                     display: button ? "none" : "block"
                 }}
             >
